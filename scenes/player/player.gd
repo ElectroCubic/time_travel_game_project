e9ee6b589e8296b6fc2e1cadeca9e6b1a57c7399
FearsomeOnion <kothariparva11@gmail.com>
2024-06-21 14:52:14 +0530

    new

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,16 +5,16 @@ var direction: Vector2
 const TILE_SIZE: int = 128
 
 func _process(_delta):
-	if Input.is_action_just_pressed("Left"):
+	if Input.is_action_pressed("Left"):
 		direction = Vector2.LEFT
 		move_player()
-	elif Input.is_action_just_pressed("Right"):
+	elif Input.is_action_pressed("Right"):
 		direction = Vector2.RIGHT
 		move_player()
-	elif Input.is_action_just_pressed("Up"):
+	elif Input.is_action_pressed("Up"):
 		direction = Vector2.UP
 		move_player()
-	elif Input.is_action_just_pressed("Down"):
+	elif Input.is_action_pressed("Down"):
 		direction = Vector2.DOWN
 		move_player()
 		
@@ -23,8 +23,8 @@ func move_player():
 		if is_moving == false:
 			is_moving = true
 			var tween = create_tween()
-			tween.tween_property(self, "position", position + (direction * TILE_SIZE), 0.3)
+			tween.tween_property(self, "position", position + (direction * TILE_SIZE), 0.5).set_ease(Tween.EASE_IN_OUT)
 			tween.tween_callback(stop_moving)
-			
+		
 func stop_moving():
 	is_moving = false
